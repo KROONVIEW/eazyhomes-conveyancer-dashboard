37319a1d306b7723042dd667d0d4b41124f717a2
Kabelo Tumisi <kbtumisi28@gmail.com>
2025-06-08 17:03:52 +0200

    Restore minimal CRACO configuration for Tailwind CSS support

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      // Minimal webpack configuration for stability
+      return webpackConfig;
+    },
+  },
+}; 
