1141fa1b965a263eb3809634eb1cdd8910f631cd
Kabelo Tumisi <kbtumisi28@gmail.com>
2025-06-08 16:32:42 +0200

    NUCLEAR FIX: Completely remove web-vitals - Build confirmed successful

--- src/reportWebVitals.js
+++
@@ -1,9 +0,0 @@
-const reportWebVitals = onPerfEntry => {
-  // Web Vitals disabled for deployment compatibility
-  // Can be re-enabled later with proper configuration
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    console.log('Web Vitals reporting is available but disabled for deployment');
-  }
-};
-
-export default reportWebVitals;
